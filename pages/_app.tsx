cd87d4addb48f0ffd52f410e8d14fbc350ad71e6
bgpark <bgpark@citus.co.kr>
2020-10-16 16:05:00 +0900

    feat: Add reset style, header

---
+++ pages/_app.tsx
@@ -0,0 +1,18 @@
+import '../css/reset.css'
+
+import Head from 'next/head'
+import React from 'react'
+
+const App = ({Component}) => {
+    return (
+        <>
+            <Head>
+                <meta charSet="utf-8" />
+                <title>여행고파</title>
+            </Head>
+            <Component/>
+        </>
+    )
+}
+
+export default App
